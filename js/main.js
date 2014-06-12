65dac6309c9b01756fab2f80bd773fed3c63faf3
unknown <Administrator@.(none)>
2014-06-13 01:04:30 +0800

    80% completed

--- js/main.js
+++ js/main.js
@@ -1,103 +1,218 @@
-/**
- * @author liege
- * date:2014-6-10 18:10:42
- */
-//飞机类
-function Plane(){
-	this.plane = null;
-	this.life = true;
-	this.armor = 1;
-	this.position = {x:0,y:0};
-	this.class = null; 
-	this.direction = "up";
-	this.bullets = null;
-}
-//显示飞机
-Plane.prototype.show = function(){
-	this.plane = document.createElement("p");
-	this.plane.score = 0;
-	this.plane.className = this.class;
-	document.getElementById("container").appendChild(this.plane);
+var game = {
+	myPlane:null,
+	allPlane:[],
+	allBullet:[],
+	planeSpeed:[4,3,2,1],
+	planeDensity:[20,200,600,1500],
+	interval:1000/24,
+	scores:0,
+	gameTime:0,
+	num:0,
+	stageBgY:0,
+	gameSet:null,
+	stage:null
 };
-
-//我机
-function myPlane(){}
-myPlane.prototype = new Plane();
-//飞机开火
-myPlane.prototype.fire = function(){
+game.initMyPlane = function(){
+	this.myPlane = new myPlane();
+	this.myPlane.class = "myPlane";
+	this.myPlane.position = {x:"45%",y:460};
+	this.myPlane.show();	
+	this.myPlane.move();
+};
+//运行游戏，一切运动皆依赖此函数
+game.run = function(){
 	var _this = this;
-	_this.position = {
-		x:_this.plane.offsetLeft + _this.plane.offsetWidth/2,
-		y:_this.plane.offsetTop - 20
-	};
-	//实例化子弹
-	var b = new Bullet();
-	_this.bullets = b;
-	//子弹方向等于飞机方向
-	_this.bullets.direction = _this.direction; 
-	//子弹初始坐标等于飞机position值
-	_this.bullets.startPos = _this.position;		
-	//子弹飞行
-	_this.bullets.fly();	
-
-};
-//飞机跟随鼠标移动
-myPlane.prototype.move = function(){
-	var _this = this,
-	stage = document.getElementById("container");
-	stage.onmouseover = function(e){
-		stage.onmousemove = function(e){
-			var E = e||event;
-			_this.position.x = E.clientX- stage.offsetLeft - _this.plane.offsetWidth/2;
-			_this.position.y = E.clientY- stage.offsetTop - _this.plane.offsetHeight/2;
-			_this.plane.style.left = _this.position.x + "px";
-			_this.plane.style.top = _this.position.y + "px";		
-			document.getElementById("t").value = _this.plane.offsetLeft;
-			if(_this.plane.offsetLeft>stage.offsetWidth-_this.plane.offsetWidth){
-				_this.plane.style.left = stage.offsetWidth-_this.plane.offsetWidth + "px";
-			}
-			if(_this.plane.offsetLeft<0){
-				_this.plane.style.left = 0;
-			}
-			if(_this.plane.offsetTop<0){
-				_this.plane.style.top = 0;
-			}
-			if(_this.plane.offsetTop>stage.offsetHeight-_this.plane.offsetHeight){
-				_this.plane.style.top = stage.offsetHeight-_this.plane.offsetHeight + "px";
-			}
-		};
-		stage.onmouseout = function(){
-			stage.onmousemove = null;
-		};
-	};
-};
-//敌机
-function npcPlane(){}
-npcPlane.prototype = new Plane();
-npcPlane.prototype.direction = "down";
-npcPlane.prototype.timer = null;
-npcPlane.prototype.speed = 2;
-npcPlane.prototype.interval = 30;
-npcPlane.prototype.autoMove = function(){
-	var _this = this;
-	_this.position.x = Math.random()*(document.getElementById("container").offsetWidth-_this.plane.offsetWidth);
-	_this.plane.style.left = _this.position.x + "px";		
-	_this.plane.style.top = _this.position.y + "px";	
-	_this.plane.armor = _this.armor;
-	_this.plane.die = function(){
-		die();
-	};
-	//飞机销毁
-	var die = function(){
-		document.getElementById("container").removeChild(_this.plane);
-		window.clearInterval(_this.timer);
-	};	
-	this.timer = window.setInterval(function(){
-		_this.plane.style.top = _this.plane.offsetTop + _this.speed + "px";
-		//判断飞机销毁或飞出界面
-		if( _this.plane.offsetTop>document.getElementById("container").offsetHeight){
-			die();
+	_this.num++;
+	_this.stage = document.getElementById("container");
+	//我机开火,push子弹		
+	
+	//
+	if(_this.num%3==0){
+		_this.myPlane.fire();
+		_this.allBullet.push(_this.myPlane.bullets);
+	// console.log(zidan.bullet)
+	}
+	
+	//获取、遍历所有子弹对象
+	var allBulletLen = _this.allBullet.length;
+	for(var i=0;i<allBulletLen;i++){
+		//判断子弹销毁或飞出界面
+		if( _this.allBullet[i].bullet.offsetTop<0){
+			_this.allBullet[i].die();			
 		}		
-	},_this.interval);
+		if(_this.allBullet[i].outside){
+			_this.allBullet.splice(i,1);
+			allBulletLen--;
+		}
+	}	
+	//创建敌机
+	if(_this.num%_this.planeDensity[0] == 0){	
+		var npc = new npcPlane();
+		npc.class = "npc1";
+		npc.armor = npc.score = 1;
+		npc.speed = _this.planeSpeed[0];
+		npc.show();
+		npc.appear();
+		_this.allPlane.push(npc);
+	}
+	if(_this.num%_this.planeDensity[1] == 0){
+		var npc = new npcPlane();
+		npc.class = "npc2";	
+		npc.armor = npc.score = 10;
+		npc.speed = _this.planeSpeed[1];	
+		npc.show();
+		npc.appear();		
+		_this.allPlane.push(npc);
+	}
+	if(_this.num%_this.planeDensity[2] == 0){
+		var npc = new npcPlane();
+		npc.class = "npc3";		
+		npc.armor = npc.score = 20;
+		npc.speed = _this.planeSpeed[2];	
+		npc.show();
+		npc.appear();
+		_this.allPlane.push(npc);		
+	}
+	if(_this.num%_this.planeDensity[3] == 0){
+		var npc = new npcPlane();
+		npc.class = "npc4";		
+		npc.armor = npc.score = 30;
+		npc.speed = _this.planeSpeed[3];	
+		npc.show();
+		npc.appear();
+		_this.allPlane.push(npc);
+		_this.num = 0;		
+	}	
+
+	//获取、遍历所有飞机对象
+	var len = _this.allPlane.length;
+	for(var i=0;i<len;i++){
+		//判断飞机销毁或飞出界面
+		if( _this.allPlane[i].plane.offsetTop>_this.stage.offsetHeight){
+			_this.allPlane[i].die();
+		}	
+		//从数组清理出界活销毁的飞机	
+		if(_this.allPlane[i].outside){
+			_this.allPlane.splice(i,1);
+			len--;
+		}
+		//我机敌机碰撞
+		if(getCollision(_this.myPlane.plane,_this.allPlane[i].plane)){
+			_this.over();
+		}
+	}
+	for(var i=0;i<_this.allPlane.length;i++){
+		_this.allPlane[i].plane.style.top = _this.allPlane[i].plane.offsetTop + _this.allPlane[i].speed + "px";
+	}
+	for(var j=0;j<_this.allBullet.length;j++){
+		_this.allBullet[j].bullet.style.top = _this.allBullet[j].bullet.offsetTop - _this.allBullet[j].speed + "px";
+		for(var i=0;i<_this.allPlane.length;i++){
+			//子弹敌机相撞
+			if(getCollision(_this.allBullet[j].bullet,_this.allPlane[i].plane)){
+				//子弹销毁
+				_this.allBullet[j].die();
+				//飞机护甲减少
+				_this.allPlane[i].armor--;
+				if(_this.allPlane[i].armor<=0){
+					//得分
+					_this.scores += _this.allPlane[i].score;
+					//飞机爆炸
+					_this.allPlane[i].bang();						
+				}				
+				return;
+			}
+		}
+	}		
+	//记分板
+	document.getElementById("score").innerHTML = "得分：" + _this.scores*100;
+	//背景运动
+	_this.stageBgY++;
+	_this.stage.style.backgroundPositionY = _this.stageBgY + "px";
+	//游戏进度
+	if(_this.scores>20 && _this.scores<40){
+		_this.planeSpeed = [5,4,3,2];
+		_this.planeDensity = [15,200,600,1500];
+	}
+	if(_this.scores>40 && _this.scores<80){
+		_this.planeSpeed = [6,5,4,3];
+		_this.planeDensity = [15,100,400,2000];
+	}
+	if(_this.scores>80 && _this.scores<100){
+		_this.planeSpeed = [7,6,5,4];
+		_this.planeDensity = [10,200,500,1500];
+	}
+	if(_this.scores>150){
+		_this.planeSpeed = [10,8,6,5];
+		_this.planeDensity = [10,150,300,1000];
+	}
+};
+game.begin = function(){
+	var _this =this;
+	_this.initMyPlane();
+	_this.gameSet = window.setInterval(function(){
+		_this.run();
+	},_this.interval);	
+};
+game.pause = function(){
+	var _this = this;
+	_this.myPlane.move(true);
+	//为何这里没法删除时间处理程序？
+	_this.myPlane.stage.onmousemove = null;
+	window.clearInterval(_this.gameSet);
+	_this.myPlane.plane.style.display = "none";
+};
+game.over = function(){
+	this.pause();
+	var info = document.getElementById("info"),
+		endScroe = document.getElementById("endScroe");
+	info.style.display = "block";
+	endScroe.innerHTML = this.scores*100;
+};
+onload = function(){
+	var begin = document.getElementById("begin"),
+		begin_btn = document.getElementById("begin_btn"),
+		pause = document.getElementById("pause"),
+		pause_btn = document.getElementById("pause_btn"),
+		again_btn = document.getElementById("again_btn"),
+		stage = document.getElementById("container");
+	begin_btn.onclick = function(e){
+		var E = e||event;
+		begin.style.display = "none";
+		game.begin();
+		E.stopPropagation();
+		E.cancelBubble = true;
+	};
+	stage.onclick = function(){
+		pause.style.display = "block";
+		game.pause();
+	};
+	pause_btn.onclick = function(e){
+		var E = e||event;	
+		game.begin();
+		pause.style.display = "none";
+		E.stopPropagation();
+		E.cancelBubble = true;		
+	};
+	again_btn.onclick = function(){
+		window.location.reload();
+	};
+	
+	
 };
 
+	//碰撞检测
+	function getCollision(obj1,obj2){
+		var l1 = obj1.offsetLeft;
+		var r1 = obj1.offsetLeft + obj1.offsetWidth;
+		var t1 = obj1.offsetTop;
+		var b1 = obj1.offsetTop+ obj1.offsetHeight;
+		var l2 = obj2.offsetLeft;
+		var r2 = obj2.offsetLeft + obj2.offsetWidth;
+		var t2 = obj2.offsetTop;
+		var b2 = obj2.offsetTop+ obj2.offsetHeight;				
+		if(r1<l2 || l1>r2 || t1>b2 || b1<t2){
+			return false;
+		}else{
+			return true;
+		}
+	}
